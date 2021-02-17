8d7358fba90dffb75340c9328b392656dc5fa7d2
Agata <polejowska.agata@gmail.com>
2021-02-17 18:16:54 +0100

    Clean up

--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 
 The website is mainly developed in HTML, CSS, JavaScript and hosted through Github. <br/>
 In orderd to experience the project entirely, I invite you to visit the following link: https://agatapolejowska.github.io
-
-This project is in progress. 
--- assets/js/main.js
+++ assets/js/main.js
@@ -38,30 +38,3 @@
 
 })(jQuery);
 
-
-function worldclock() {
-    var date = new Date();
-    var h = date.getHours();  // 0-23
-    var m = date.getMinutes(); // 0-59
-    var s = date.getSeconds(); // 0-59
-    var session = "AM";
-
-    if(h == 0) {
-      h = 12;
-    }
-    if(h > 12) {
-      h = h -12;
-      session = "PM";
-    }
-    
-    h=(h<10) ? "0" +h:h;
-    m=(m<10) ? "0" +m:m;
-    s=(s<10) ? "0" +s:s;
-
-    var time = h + " : " + m + " : " + s +" "+ session;
-
-    document.getElementById("showTime").innerText = time;
-    document.getElementById("showTime").textContent = time;
-    setTimeout(worldclock, 1000);
-    }
-    worldclock();
